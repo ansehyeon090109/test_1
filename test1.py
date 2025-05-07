47c2c83fe2d3e60569db7ac98d2a67c265a45eef
홍길동 <ansehyeon090109@gmail.com>
2025-05-07 20:10:25 +0900

    수정3

--- test1.py
+++ test1.py
@@ -1,4 +1,10 @@
 import streamlit as st 
 
 col1, col2 = st.columns([2,3])
+with col1 :
+    st.title("Here is column1 title")
+    st.checkbox("this is checkbox1 in col1")
 
+with col2 :
+    st.title("Here is column2 title")
+    st.checkbox("this is checkbox1 in col2")
